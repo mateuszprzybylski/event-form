65fcff5caefd9c6404c140455bbf34ce431c3c29
Mateusz Przybylski <poczta.mateusz.przybylski@gmial.com>
2019-02-16 14:45:33 +0100

    setup enzyme tests

---
+++ src/components/AppHeader/AppHeader.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import AppHeader from './AppHeader';
+
+
+describe('AppHeader ', () => {
+    it('should renders without crashing', () => {
+        shallow(<AppHeader />);
+    });
+
+    it('should contains header text', () => {
+        const headerText = 'Example header';
+        const appHeader = shallow(<AppHeader header={headerText} />);
+
+        expect(appHeader.text()).toContain(headerText);
+    });
+});
