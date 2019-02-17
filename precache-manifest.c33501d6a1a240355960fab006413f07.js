a3027ae3cef0ca570041f48ad653abb6c7fbc425
Mateusz Przybylski <poczta.mateusz.przybylski@gmial.com>
2019-02-17 23:11:46 +0100

    Updates

---
+++ precache-manifest.c33501d6a1a240355960fab006413f07.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ade93e8c9737cb8c51f",
+    "url": "/event-form/static/css/main.27fe7a8a.chunk.css"
+  },
+  {
+    "revision": "0ade93e8c9737cb8c51f",
+    "url": "/event-form/static/js/main.0ade93e8.chunk.js"
+  },
+  {
+    "revision": "cfe8308829148831bb88",
+    "url": "/event-form/static/js/runtime~main.cfe83088.js"
+  },
+  {
+    "revision": "3930fd0394615da53a4e",
+    "url": "/event-form/static/js/2.3930fd03.chunk.js"
+  },
+  {
+    "revision": "297a70c8161b6afd85353ec833ed0d72",
+    "url": "/event-form/index.html"
+  }
+];
